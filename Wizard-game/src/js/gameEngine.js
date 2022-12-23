b211974f6aee9f33e183d39643c5a97a1427eb2b
MonikaSimeonova <118007413+MonikaSimeonova@users.noreply.github.com>
2022-12-23 20:41:16 +0200

    wizard game

--- Wizard-game/src/js/gameEngine.js
+++ Wizard-game/src/js/gameEngine.js
@@ -1,36 +1,113 @@
-function start(state,game){
+function start(state, game) {
     game.createWizard(state.wizard);
 
-    window.requestAnimationFrame(gameLoop.bind(null,state,game))
+    window.requestAnimationFrame(timestamp => gameLoop(state, game, timestamp))
 }
 
-function gameLoop(state,game){
-    const {wizard} = state;
-    const {wizardElement} = game;
-    //Move wizard
-    if(state.keys.KeyD){
-        wizard.posX = Math.min(wizard.posX + wizard.speed, game.gameScreen.offsetWidth - wizard.width)
+function gameLoop(state, game, timestamp) {
+    const { wizard } = state;
+    const { wizardElement } = game;
+    game.scoreScreen.textContent = `${state.score} pts.`
+    
+
+    modifyWizardPosition(state, game)
+
+    if (state.keys.Space) {
+        game.wizardElement.style.backgroundImage = 'url("/src/images/wizard-fire.png")';
+        if(timestamp > state.fireball.nextSpawnTimestamp){
+        game.createFireball(wizard, state.fireball);
+        state.fireball.nextSpawnTimestamp = timestamp + state.fireball.fireRate
+
+        }
+    } else {
+        game.wizardElement.style.backgroundImage = 'url("/src/images/wizard.png")'
 
     }
-    if(state.keys.KeyW && wizard.posY > 0){
-        wizard.posY = Math.max(wizard.posY - wizard.speed, 0)
-        
-    }
-    if(state.keys.KeyA){
-        wizard.posX = Math.max(wizard.posX - wizard.speed, 0)
-    }
-    if(state.keys.KeyS){
-        wizard.posY = Math.min(wizard.posY + wizard.speed, game.gameScreen.offsetHeight - wizard.height)
+
+
+    //spawn bugs
+
+    if (timestamp > state.bugStats.nextSpawnTimestamp) {
+        game.createBug(state.bugStats);
+        state.bugStats.nextSpawnTimestamp = timestamp + Math.random() * state.bugStats.maxSpawnInterval;
     }
 
+    //render bugs
+    let bugElements = document.querySelectorAll('.bug');
+    bugElements.forEach(bug => {
+        let posX = parseInt(bug.style.left); // from 400px to 400
+
+        if(detectCollision(wizardElement,bug)){
+            state.gameOver = true;
+
+        }
+        if (posX > 0) {
+            bug.style.left = posX - state.bugStats.speed + 'px';
+
+        } else {
+            bug.remove()
+        }
+
+    })
+    //render fireball
+    document.querySelectorAll('.fireball').forEach(fireball => {
+        let posX = parseInt(fireball.style.left);
+
+        // detect collision
+        bugElements.forEach(bug=>{
+            if(detectCollision(bug,fireball)){
+                state.score += state.killScore
+                bug.remove();
+                fireball.remove();
+            }
+        })
+        if (posX > game.gameScreen.offsetWidth) {
+            fireball.remove()
+        } else {
+            fireball.style.left = posX + state.fireball.speed + 'px'
+
+        }
+    })
 
     //render wizard
     wizardElement.style.left = wizard.posX + 'px';
     wizardElement.style.top = wizard.posY + 'px';
-  
+
+    if(state.gameOver){
+        alert(`GAME OVER - You had ${state.score} pts!`)
+    }else{
+        state.score += state.scoreRate;
+    window.requestAnimationFrame(gameLoop.bind(null, state, game))
+
+    }
 
 
+}
+function modifyWizardPosition(state, game) {
+    const { wizard } = state;
 
+    //Move wizard
+    if (state.keys.KeyD) {
+        wizard.posX = Math.min(wizard.posX + wizard.speed, game.gameScreen.offsetWidth - wizard.width)
 
-    window.requestAnimationFrame(gameLoop.bind(null,state,game))
+    }
+    if (state.keys.KeyW && wizard.posY > 0) {
+        wizard.posY = Math.max(wizard.posY - wizard.speed, 0)
+
+    }
+    if (state.keys.KeyA) {
+        wizard.posX = Math.max(wizard.posX - wizard.speed, 0)
+    }
+    if (state.keys.KeyS) {
+        wizard.posY = Math.min(wizard.posY + wizard.speed, game.gameScreen.offsetHeight - wizard.height)
+    }
+}
+
+function detectCollision(objectA,objectB){
+    let first = objectA.getBoundingClientRect();
+    let second = objectB.getBoundingClientRect();
+
+    let hasCollision = !(first.top > second.bottom || first.bottom < second.top || first.right < second.left || first.left > second.right);
+
+    return hasCollision
 }
--- Wizard-game/src/js/gameState.js
+++ Wizard-game/src/js/gameState.js
@@ -3,6 +3,10 @@ function initState() {
     let startY = Math.floor(Math.random() * 500);
     const state = {
         player: 'Moni',
+        gameOver: false,
+        score: 0,
+        scoreRate: 1,
+        killScore: 1000,
 
         wizard: {
             width: 100,
@@ -12,7 +16,29 @@ function initState() {
             speed: 10,
 
         },
-        keys: {}
+        bugStats: {
+            width: 50,
+            height: 50,
+            nextSpawnTimestamp: 0,
+            maxSpawnInterval: 1500,
+            speed: 8,
+        },
+        fireball:{
+            width: 20,
+            height: 20,
+            speed: 12,
+            nextSpawnTimestamp: 0,
+            fireRate: 500,
+        },
+        keys: {
+            KeyA: false,
+            KeyS: false,
+            KeyD: false,
+            KeyW: false,
+            Space: false,
+           
+        },
+        
     }
     return state;
 
